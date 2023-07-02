56c4db7b3a1602883244ab62c0dcf2896afd2c9d
Suraj Pal <king007suraj@gmail.com>
2023-07-02 21:49:53 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/gomoon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gomoon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:gomoon/widgets/custom_dropdown_button.dart';
+
+class HomePage extends StatelessWidget {
+  late double _deviceHeight, _deviceWidth;
+
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: _deviceHeight,
+          width: _deviceWidth,
+          padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _pageTitle(),
+                  _bookRideWidget(),
+                ],
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child:_astroImageWidget() ,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+          color: Colors.white, fontSize: 70, fontWeight: FontWeight.w800),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      height: _deviceHeight * 0.50,
+      width: _deviceWidth * 0.65,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.contain,
+          image: AssetImage("assets/images/astro_moon.png"),
+        ),
+      ),
+    );
+  }
+
+  Widget _destinationDropDownWidget() {
+    return CustomDropDownButtonClass(
+      values: ['ISRO Station', 'NASA Station', 'Russian'],
+      width: _deviceWidth,
+    );
+  }
+
+  Widget _travellersInformationDropDownWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CustomDropDownButtonClass(
+          values: ['1', '2', '3'],
+          width: _deviceWidth * 0.45,
+        ),
+        CustomDropDownButtonClass(
+          values: ['Economy', 'Business', 'Private'],
+          width: _deviceWidth * 0.40,
+        ),
+      ],
+    );
+  }
+
+  Widget _bookRideWidget() {
+    return Container(
+      height: _deviceHeight * 0.25,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _destinationDropDownWidget(),
+          _travellersInformationDropDownWidget(),
+          _rideButton()
+        ],
+      ),
+    );
+  }
+
+  Widget _rideButton() {
+    return Container(
+      margin: EdgeInsets.only(bottom: _deviceHeight * 0.01),
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        child: const Text(
+          "Book ride!",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
